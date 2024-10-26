9502adf550ff381f4bf68a185fc27d3a0f37e2a5
sergeytashkinov <tashkinov2@gmail.com>
2024-10-26 21:56:56 +0300

    media fixed

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mailings.urls',namespace='mailings')),
     path('', include('users.urls',namespace='users')),
     path('', include('blog.urls',namespace='blog')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
